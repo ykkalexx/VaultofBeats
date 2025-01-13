81e40c95f3268cc47c1e50dcaa912ee140d9a7f5
alex-b23 <138300434+alex-b23@users.noreply.github.com>
2025-01-13 13:19:07 +0000

    connected AWS to backend for uploading the music file

---
+++ Server/src/services/AWSService.js
@@ -0,0 +1,39 @@
+import AWS from "aws-sdk";
+
+export class AWSService {
+    constructor() {
+        this.s3 = new AWS.S3();
+    }
+
+    // Upload to S3
+    async uploadToS3(file, fileName) {
+        try {
+            const params = {
+                Body: file,
+                Bucket: process.env.AWS_BUCKET,
+                Key: fileName,
+            };
+
+            await this.s3.upload(params).promise();
+        } catch (error) {
+            console.error(`Failed to upload ${fileName} to S3: ${error}`);
+            throw new Error("Failed to upload to S3: " + error.message);
+        }
+    }
+
+    // Function to fetch file from S3
+    async fetchFromS3(fileName) {
+        try {
+            const params = {
+                Bucket: process.env.AWS_BUCKET,
+                Key: fileName,
+            };
+
+            const data = await this.s3.getObject(params).promise();
+            return data.Body; // Return the file content
+        } catch (error) {
+            console.error(`Failed to download ${fileName} from S3: ${error}`);
+            throw new Error("Failed to download from S3: " + error.message);
+        }
+    }
+}
